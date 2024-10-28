8d26703e27ef651af0cf471edc6cd9467f0f3bab
ashudevcodes <ashishprasad949@gmail.com>
2024-10-28 18:50:12 +0530

    Ugly looking app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,22 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs screenOptions={{ tabBarActiveTintColor: 'black', headerShown: false }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    tabBarIcon: ({ color }) => <FontAwesome size={30} name="male" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="account"
+                options={{
+                    tabBarIcon: ({ color }) => <FontAwesome size={30} name="paw" color={color} />,
+                }}
+            />
+        </Tabs>
+    );
 }
+
---
+++ app/(tabs)/account.tsx
@@ -0,0 +1,11 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function Account() {
+    return (
+        <ThemedView>
+            <ThemedText>This is a Account Page</ThemedText>
+        </ThemedView>
+    )
+
+}
